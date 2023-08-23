10e348a179085b66c41ff5a584d79e22589d95cd
Yusuf Azam <yusufazam95@hotmail.co.uk>
2023-08-22 19:31:32 -0500

    starting to add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
-import './App.css';
-import { items } from './items';
+import React, { useEffect, useState } from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import axios from 'axios';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import {
+  BrowserRouter as Router,
+} from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/items').then((result) => setItems(result.data))
+      // eslint-disable-next-line no-console
+      .catch(console.error);
+  }, []);
   return (
-    <div>
+    <Router>
       <Header />
       <Home items={items} />
-    </div>
+    </Router>
   );
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import './Home.css';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import PropTypes from 'prop-types';
 import Thumbnail from './Thumbnail';
 import { itemImages } from '../items';
 import ItemType from '../types/item';
 
-// eslint-disable-next-line react/prop-types
 function Home({ items }) {
   return (
     <div className="home-component">
